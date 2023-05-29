8052b535b6078c66f8ec0664458bd177be7deb16
Juan518 <juanmunoz890@gmail.com>
2023-05-29 10:43:02 -0300

    removed useless id inside utxos

--- src/utxo.rs
+++ src/utxo.rs
@@ -1,6 +1,4 @@
 use crate::raw_transaction::{RawTransaction, TxOutput};
-use crate::utility::double_hash;
-use bitcoin_hashes::Hash;
 use std::io;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -26,22 +24,19 @@ pub type UtxoId = [u8; 32];
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Utxo {
-    pub txid: UtxoId,
-    transaction: Vec<UtxoTransaction>,
+    transactions: Vec<UtxoTransaction>,
 }
 
 impl Utxo {
     pub fn _from_raw_transaction(raw_transaction: &RawTransaction) -> io::Result<Utxo> {
-        let txid = double_hash(&raw_transaction.serialize()).to_byte_array();
 
         let mut utxo = Utxo {
-            txid,
-            transaction: Vec::new(),
+            transactions: Vec::new(),
         };
 
         for tx_output in &raw_transaction.tx_out {
             let utxo_transaction = UtxoTransaction::_from_tx_output(tx_output)?;
-            utxo.transaction.push(utxo_transaction);
+            utxo.transactions.push(utxo_transaction);
         }
         Ok(utxo)
     }
@@ -50,7 +45,7 @@ impl Utxo {
     /// and mark it as spent
     pub fn _validate_spend(&self, index: usize) -> io::Result<()> {
         // first check that it exists
-        if index >= self.transaction.len() {
+        if index >= self.transactions.len() {
             println!("Utxo index out of bounds!");
             // return Err(io::Error::new(
             //     io::ErrorKind::InvalidInput,
